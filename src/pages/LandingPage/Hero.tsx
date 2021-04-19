834c870e45b83b5de49e9dc46998b7c9ad0331bc
Bamidele Isaac <bamideleisaac11@gmail.com>
2021-04-19 04:33:00 +0100

    landing page

--- src/pages/LandingPage/Hero.tsx
+++ src/pages/LandingPage/Hero.tsx
@@ -14,7 +14,7 @@ export const LandingHero = (props: Props) => {
       <div className="row">
         <div className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h2>We are talented team of professionals promoting a productive employment and decent work for all</h2>
           <div className="d-flex justify-content-center justify-content-lg-start align-items-center" >
 		  <Form.Control size="lg" type="text" placeholder="Find Employee" />
             <a href="/applicant" className="btn-get-started scrollto">Go</a>
